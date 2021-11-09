d17a450c7627d83d11fe90a2445a309fd583f51f
origogi <rlawjdxo1212@naver.com>
2021-11-09 13:37:26 +0900

    add Material component

--- week 2-1-Layouts in JetpackCompose/myCodeLab/app/src/main/java/com/origogi/mycodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/myCodeLab/app/src/main/java/com/origogi/mycodelab/MainActivity.kt
@@ -3,14 +3,19 @@ package com.origogi.mycodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,15 +26,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyCodeLabTheme {
-
+//                PhotoGrapherCard()
+                MyCodeLab()
             }
         }
+    }
+
+
+}
+
+@Composable
+fun MyCodeLab() {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text(text = "LayoutsCodeLab")
+            }, actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = "")
+                }
+            })
+
+    }) { innerPadding ->
+        BodyContent(innerPadding)
     }
 }
 
 @Composable
-fun PhotoGrapherCard() {
-    Row {
+private fun BodyContent(innerPadding: PaddingValues) {
+    Column(modifier = Modifier.padding(innerPadding)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layout codelab")
+    }
+}
+
+@Composable
+fun PhotoGrapherCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable {
+
+            }
+            .padding(16.dp)
+    ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
@@ -37,12 +79,24 @@ fun PhotoGrapherCard() {
         ) {
 
         }
-        Column() {
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
             Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text("3 minutes ago", style = MaterialTheme.typography.body2)
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun MyCodeLabPreview() {
+    MyCodeLabTheme {
+        MyCodeLab()
     }
 }
 
