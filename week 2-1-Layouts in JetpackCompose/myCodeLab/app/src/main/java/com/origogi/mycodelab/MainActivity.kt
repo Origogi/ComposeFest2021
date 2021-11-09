d6a16d5e75337f2e9d17bbf7f09a9c50e47fa5bf
origogi <rlawjdxo1212@naver.com>
2021-11-09 16:57:03 +0900

    add

--- week 2-1-Layouts in JetpackCompose/myCodeLab/app/src/main/java/com/origogi/mycodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/myCodeLab/app/src/main/java/com/origogi/mycodelab/MainActivity.kt
@@ -3,9 +3,7 @@ package com.origogi.mycodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -20,8 +18,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.origogi.mycodelab.ui.theme.MyCodeLabTheme
@@ -34,12 +38,11 @@ class MainActivity : ComponentActivity() {
             MyCodeLabTheme {
 //                PhotoGrapherCard()
 //                MyCodeLab()
-                ScrollingList()
+//                ScrollingList()
+                BodyContent()
             }
         }
     }
-
-
 }
 
 @Composable
@@ -131,7 +134,7 @@ fun ScrollingList() {
             }
             Button(onClick = {
                 coroutineScope.launch {
-                    scrollState.animateScrollToItem(listSize -1)
+                    scrollState.animateScrollToItem(listSize - 1)
                 }
             }) {
                 Text(text = "Scroll to the end")
@@ -143,6 +146,167 @@ fun ScrollingList() {
                 ImageListItem(index = it)
             }
         }
+    }
+}
+
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints = constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseLine = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseLine
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Composable
+fun StaggeredGrid(modifier: Modifier, rows: Int = 3, content: @Composable () -> Unit) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { i, measurable ->
+            val placeable = measurable.measure(constraints = constraints)
+            val row = i % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minWidth..constraints.maxWidth)
+            ?: constraints.minWidth
+        val height = rowHeights.sum().coerceIn(constraints.minHeight..constraints.maxHeight)
+
+        val rowY = IntArray(rows) { 0 }
+
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { i, placeable ->
+                val row = i % rows
+                placeable.placeRelative(
+                    rowX[row],
+                    rowY[row]
+                )
+                rowX[row] += placeable.width
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text)
+        }
+    }
+}
+
+@Composable
+fun MyOwnColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var positionY = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = positionY)
+                positionY += placeable.height
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    MyCodeLabTheme() {
+        Chip(text = "Hi there")
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+
+        StaggeredGrid(modifier = modifier) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    MyCodeLabTheme() {
+        BodyContent()
+    }
+}
+
+//@Preview
+//@Composable
+//fun BodyContent(modifier: Modifier = Modifier) {
+//    MyOwnColumn(modifier.padding(8.dp)) {
+//        Text("MyOwnColumn")
+//        Text("places items")
+//        Text("vertically.")
+//        Text("We've done it by hand!")
+//    }
+//}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    MyCodeLabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    MyCodeLabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
